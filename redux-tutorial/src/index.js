e35931e510a782e2893518df1cfc6721123724c0
Ivan De Vita <ivandevita1995@gmail.com>
2019-04-05 17:51:50 +0200

    primo step redux

---
+++ redux-tutorial/src/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+import { createStore } from 'redux';
+
+function reducer(state, action) { // 2. create function reducer // 5. update the store - reducer
+    if(action.type === 'changeState'){
+        return action.payload.newState;
+    }
+    return 'State';
+}
+
+const store = createStore(reducer); // 1. create the store
+ 
+console.log(store.getState()); // 3. console store 
+
+const action = { // 3. action declaration
+    type: 'changeState',
+    payload: {
+        newState: 'New state'
+    }
+}
+
+store.dispatch(action); // 4. dispatch an action
+
+console.log(store.getState()) // 6. stampa
+
+
+
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
